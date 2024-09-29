dcb3e5bc1377cf383fc9ec63a63fd885c8cb9800
yosukeN <yousken72@gmail.com>
2024-09-30 05:28:40 +0900

    fix. arguments for subprocess

--- src/put_object_s3.py
+++ src/put_object_s3.py
@@ -5,7 +5,7 @@ import subprocess
 
 def show_s3_ls_object():
     output_str = subprocess.run(
-        'aws s3 ls',
+        ["aws","s3","ls"],
         capture_output=True,
         text=True,
     ).stdout
