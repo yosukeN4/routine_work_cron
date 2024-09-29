aec6b0833c07c0e54f0ca6c22d82178752eecd6c
yosukeN <yousken72@gmail.com>
2024-09-30 05:00:57 +0900

    fix. use subprocess.run

--- src/put_object_s3.py
+++ src/put_object_s3.py
@@ -5,9 +5,12 @@ import subprocess
 
 
 def put_s3_object():
-    output_str = subprocess.call(
+    output_str = subprocess.run(
         f"aws s3 ls",
         capture_output=True,
         text=True,
     ).stdout
     print(output_str)
+
+if __name__ == '__main__':
+    put_s3_object()
